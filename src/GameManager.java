78856d4d90d915e0a23c7228772e8928922c924b
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-10-23 10:56:02 +0200

    Declare method promptCreatePlayer()

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+
 public class GameManager {
 
+    private Player player;
+    private Scanner scanner = new Scanner(System.in);
+
+    public GameManager() {
+
+    }
+
+    public void run() {
+        System.out.println("\n\n====================== WELCOME TO MINESWEEPER! ==========================\n\n");
+        this.promptCreatePlayer();
+    }
+
+    /**
+     * Asks user to enter name. Creates a Player instance.
+     */
+    private void promptCreatePlayer() {
+        System.out.println("Please enter your name:");
+
+        boolean validNameInput = false;
+
+        while(!validNameInput) {
+            String playerName = scanner.nextLine();
+            if (playerName.isEmpty()) {
+                System.out.println("Enter a valid name!");
+            } else {
+                player = new Player(playerName);
+                validNameInput = true;
+            }
+        }
+    }
 }
